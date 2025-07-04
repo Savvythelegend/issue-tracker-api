782a28bc5ce77d8a1b129f076c7d3df017a132bc
Mehfooj Alam <ml.mehfooj@gmail.com>
2025-07-04 10:03:10 +0000

    chore(cleanup): remove unwanted build, coverage, and db files from version control

--- build/lib/app/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flasgger import Swagger
-from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-jwt = JWTManager()
-swagger = Swagger()
-migrate = Migrate()
